e620f2ea8039df37a73c261db4264cd2a9d5d104
Sergei Zaiaev <serjzv@gmail.com>
2024-08-28 15:33:17 +0300

    Manage user membership in the mapper

--- src/main/java/WaldurOIDCOfferingAccessMapper.java
+++ src/main/java/WaldurOIDCOfferingAccessMapper.java
@@ -11,9 +11,13 @@ import java.util.Map;
 import org.apache.http.HttpHeaders;
 import org.jboss.logging.Logger;
 import org.keycloak.models.ClientSessionContext;
+import org.keycloak.models.GroupModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.ProtocolMapperModel;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
 import org.keycloak.models.UserSessionModel;
+import org.keycloak.models.utils.KeycloakModelUtils;
 import org.keycloak.protocol.oidc.OIDCLoginProtocol;
 import org.keycloak.protocol.oidc.mappers.AbstractOIDCProtocolMapper;
 import org.keycloak.protocol.oidc.mappers.OIDCAccessTokenMapper;
@@ -42,53 +46,47 @@ public class WaldurOIDCOfferingAccessMapper extends AbstractOIDCProtocolMapper
     private static final String PROVIDER_UUID_KEY = "uuid.waldur.provider.value";
     private static final String GROUP_NAME_KEY = "name.keycloak.group.value";
 
-
     static {
         ProviderConfigProperty property;
 
         property = new ProviderConfigProperty(
-            API_URL_KEY,
-            "Waldur API URL",
-            "URL to the Waldur API including trailing backslash, e.g. https://waldur.example.com/api/",
-            ProviderConfigProperty.STRING_TYPE,
-            ""
-        );
+                API_URL_KEY,
+                "Waldur API URL",
+                "URL to the Waldur API including trailing backslash, e.g. https://waldur.example.com/api/",
+                ProviderConfigProperty.STRING_TYPE,
+                "");
         configProperties.add(property);
 
         property = new ProviderConfigProperty(
-            PROVIDER_UUID_KEY,
-            "Waldur provider UUID",
-            "UUID of the provider in Waldur",
-            ProviderConfigProperty.STRING_TYPE,
-            ""
-        );
+                PROVIDER_UUID_KEY,
+                "Waldur provider UUID",
+                "UUID of the provider in Waldur",
+                ProviderConfigProperty.STRING_TYPE,
+                "");
         configProperties.add(property);
 
         property = new ProviderConfigProperty(
-            OFFERING_UUID_KEY,
-            "Waldur offering UUID",
-            "UUID of the offering in Waldur",
-            ProviderConfigProperty.STRING_TYPE,
-            ""
-        );
+                OFFERING_UUID_KEY,
+                "Waldur offering UUID",
+                "UUID of the offering in Waldur",
+                ProviderConfigProperty.STRING_TYPE,
+                "");
         configProperties.add(property);
 
         property = new ProviderConfigProperty(
-            API_TOKEN_KEY,
-            "Waldur API token",
-            "Token for Waldur API",
-            ProviderConfigProperty.STRING_TYPE,
-            ""
-        );
+                API_TOKEN_KEY,
+                "Waldur API token",
+                "Token for Waldur API",
+                ProviderConfigProperty.STRING_TYPE,
+                "");
         configProperties.add(property);
 
         property = new ProviderConfigProperty(
-            GROUP_NAME_KEY,
-            "Keycloak group name.",
-            "Name of the precreated group in Keycloak.",
-            ProviderConfigProperty.STRING_TYPE,
-            ""
-        );
+                GROUP_NAME_KEY,
+                "Keycloak group name.",
+                "Name of the precreated group in Keycloak.",
+                ProviderConfigProperty.STRING_TYPE,
+                "");
         configProperties.add(property);
 
         OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, WaldurOIDCOfferingAccessMapper.class);
@@ -129,7 +127,7 @@ public class WaldurOIDCOfferingAccessMapper extends AbstractOIDCProtocolMapper
     }
 
     private boolean hasAccessToResource(String waldurUrl, String offeringUuid, String waldurToken, String username) {
-        if (offeringUuid.equals("")){
+        if (offeringUuid.equals("")) {
             LOGGER.error("Offering UUID is empty, skipping resource access check");
             return false;
         }
@@ -167,28 +165,52 @@ public class WaldurOIDCOfferingAccessMapper extends AbstractOIDCProtocolMapper
         return false;
     }
 
-    @Override
-    protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession,
-            KeycloakSession keycloakSession,
-            ClientSessionContext clientSessionCtx) {
-
+    private void transformToken(IDToken token, Map<String, String> config, KeycloakSession keycloakSession,
+            UserSessionModel userSession) {
         String username = userSession.getUser().getUsername();
 
-        final String waldurUrl = mappingModel.getConfig().get(API_URL_KEY);
-        final String providerUuid = mappingModel.getConfig().get(PROVIDER_UUID_KEY);
-        final String offeringUuid = mappingModel.getConfig().get(OFFERING_UUID_KEY);
-        final String groupName = mappingModel.getConfig().get(GROUP_NAME_KEY);
-        final String waldurToken = mappingModel.getConfig().get(API_TOKEN_KEY);
+        final String waldurUrl = config.get(API_URL_KEY);
+        final String providerUuid = config.get(PROVIDER_UUID_KEY);
+        final String offeringUuid = config.get(OFFERING_UUID_KEY);
+        final String groupName = config.get(GROUP_NAME_KEY);
+        final String waldurToken = config.get(API_TOKEN_KEY);
+
+        UserModel user = userSession.getUser();
+        RealmModel realm = keycloakSession.getContext().getRealm();
+        String groupPath = String.format("/%s", groupName);
+        GroupModel group = KeycloakModelUtils.findGroupByPath(realm, groupName);
+
+        if (group == null) {
+            LOGGER.error(String.format("The group %s (path %s) does not exist, skipping user processing.",
+                    groupName,
+                    groupPath));
+            return;
+        }
 
         boolean hasRelatedOfferingUser = this.hasRelatedOfferingUser(waldurUrl, providerUuid, waldurToken, username);
         if (!hasRelatedOfferingUser)
             return;
-        boolean hasAccessToResoure = this.hasAccessToResource(waldurUrl, offeringUuid, waldurToken, username);
-        final String groupName = config.get(OIDCAttributeMapperHelper.TOKEN_CLAIM_NAME);
+        boolean hasAccessToResource = this.hasAccessToResource(waldurUrl, offeringUuid, waldurToken, username);
 
-        token.getOtherClaims().put(claimName, username);
-        if (hasAccessToResoure)
-            token.getOtherClaims().put("groups", GROUP_NAME);
+        if (hasAccessToResource) {
+            if (!user.isMemberOf(group)) {
+                LOGGER.info(String.format("The user %s is already in the group %s", user.getUsername(), group.getName()));
+            }
+            else {
+                LOGGER.info(String.format("Adding user %s to group %s", user.getUsername(), group.getName()));
+                user.joinGroup(group);
+            }
+        } else if(user.isMemberOf(group)) {
+            LOGGER.info(String.format("Removing user %s to group %s", user.getUsername(), group.getName()))
+        }
+    }
+
+    @Override
+    protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession,
+            KeycloakSession keycloakSession, ClientSessionContext clientSessionCtx) {
+        Map<String, String> config = mappingModel.getConfig();
+
+        this.transformToken(token, config, keycloakSession, userSession);
     }
 
     public static ProtocolMapperModel create(
